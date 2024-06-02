3fdc17acc76095eab18e3be236b3f30cdb8c544f
alkhair <alkhair.mohammd@gmail.com>
2024-06-02 14:44:47 +0200

    rest of 15-1. Cubes + 15-2. Colored Cubes.py

--- exersices/15-1. Cubes + 15-2. Colored Cubes.py
+++ exersices/15-1. Cubes + 15-2. Colored Cubes.py
@@ -1,17 +1,18 @@
 import matplotlib.pyplot as plt
 
-numbers = [1, 2, 3, 4, 5]
+numbers = range(1, 5001)
 cubes = [x**3 for x in numbers]
 
 fig, ax = plt.subplots()
-ax.plot(numbers, cubes, linewidth=3)
+ax.scatter(numbers, cubes, s=3, c=cubes, cmap=plt.cm.Reds)
 
 # Set chart title and label axis
 ax.set_title("Cubes Numbers", fontsize=24)
 ax.set_xlabel("Values", fontsize=14)
 ax.set_ylabel("Cubes of Values", fontsize=14)
 
-# Set size of tick label
+# Set size of tick label and axis' rang
 ax.tick_params(axis='both', labelsize=14)
+ax.axis([0, 5100, 0, 130000000000])
 
 plt.show()
