c096625c9d11be824fd79497d3f42a0b7baf2f29
alkhair <alkhair.mohammd@gmail.com>
2024-06-04 14:56:31 +0200

    styling the walk

--- rw_visual.py
+++ rw_visual.py
@@ -5,13 +5,24 @@ from random_walk import RandomWalk
 # keep making new walks, as long as the program is active.
 while True:
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, c='red')
+    fig, ax = plt.subplots(figsize=(13, 7))
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers,
+               cmap=plt.cm.Reds, edgecolors='none', s=1)
+
+    # Emphasize the first and last point.
+    ax.scatter(0, 0, c='yellow', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='orange',
+               edgecolors='none', s=100)
+
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
     plt.show()
 
