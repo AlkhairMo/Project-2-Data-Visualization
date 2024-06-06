a95cbb431a6f3b01c58b0ba1588812e8d698164a
alkhair <alkhair.mohammd@gmail.com>
2024-06-06 14:34:02 +0200

    making a histogram of two D8 dice 20,000 rolls

--- dice_visual.py
+++ dice_visual.py
@@ -4,12 +4,12 @@ from plotly import offline
 from die import Die
 
 # Create a D6 and a D10 dice.
-die_1 = Die()
-die_2 = Die(10)
+die_1 = Die(8)
+die_2 = Die(8)
 
 # Make some rolls, and store results in a list.
 results = []
-for roll_num in range(50_000):
+for roll_num in range(20_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -26,7 +26,7 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling a D6 and a D10 dice 50,000 times: ',
+my_layout = Layout(title='Results of rolling two D8 dice 20,000 times: ',
                    xaxis=x_axis_config, yaxis=y_axis_config)
 offline.plot({'data': data, 'layout': my_layout},
-             filename='d6_d10.html')
+             filename='d8_d8.html')
