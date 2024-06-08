ff62596114d93f933d18f76fad1e9d22f3225db9
alkhair <alkhair.mohammd@gmail.com>
2024-06-08 17:28:32 +0200

    plotting data of high temperature in a Temperature Chart

--- sitrka_highs.py
+++ sitrka_highs.py
@@ -1,5 +1,7 @@
 import csv
 
+import matplotlib.pyplot as plt
+
 filename = 'data/sitka_weather_07-2021_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
@@ -8,4 +10,15 @@ with open(filename) as f:
     # Get high temperatures from this file.
     highs = [int(row[4]) for row in reader]
 
-    print(highs)
+# Plot the high temperatures.
+plt.style.use('Solarize_Light2')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# Format plot.
+plt.title("Daily high temperatures, July 2021", fontsize=18)
+plt.xlabel('', fontsize=13)
+plt.ylabel("Temperature (F)", fontsize=13)
+plt.tick_params(axis='both', which='major', labelsize=13)
+
+plt.show()
