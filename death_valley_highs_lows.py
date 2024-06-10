925a34cbb61c0aabec401ce7a971f4a6060a5471
alkhair <alkhair.mohammd@gmail.com>
2024-06-10 14:54:26 +0200

    plotting high low temperatures for death valley, CA

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-filename = 'data/sitka_weather_2021_simple.csv'
+filename = 'data/death_valley_2021_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -12,11 +12,15 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         date = datetime.strptime(row[2], '%Y-%m-%d')
-        high = int(row[4])
-        low = int(row[5])
-        dates.append(date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[3])
+            low = int(row[4])
+        except ValueError:
+            print(f"Missing data for {date}")
+        else:
+            dates.append(date)
+            highs.append(high)
+            lows.append(low)
 
 # Plot the high and low temperatures.
 plt.style.use('Solarize_Light2')
@@ -26,7 +30,7 @@ ax.plot(dates, lows, c='blue', linewidth=0.8, alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-plt.title("Daily high and low temperatures - 2021\nSitka, Alaska", fontsize=14)
+plt.title("Daily high and low temperatures - 2021\nDeath Valley, CA", fontsize=14)
 plt.xlabel('', fontsize=13)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=13)
