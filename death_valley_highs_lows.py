7f1335fd13181cf8f52df533b216edb8c5c0e4f3
alkhair <alkhair.mohammd@gmail.com>
2024-06-11 17:36:42 +0200

    make automatic indexes for temperature and rainfall codes (Exercise 16-4)

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -3,24 +3,29 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-filename = 'data/death_valley_2021_simple.csv'
+filename = 'data/death_valley_2021_full.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
+    headers = {}
+    for index, column in enumerate(header_row):
+        headers[column] = index
+
     # Get date, and high and low temperatures from this file.
     dates, highs, lows = [], [], []
     for row in reader:
-        date = datetime.strptime(row[2], '%Y-%m-%d')
+        date = datetime.strptime(row[headers['DATE']], '%Y-%m-%d')
         try:
-            high = int(row[3])
-            low = int(row[4])
+            high = int(row[headers['TMAX']])
+            low = int(row[headers['TMIN']])
         except ValueError:
             print(f"Missing data for {date}")
         else:
             dates.append(date)
             highs.append(high)
             lows.append(low)
+    station = row[headers['NAME']]
 
 # Plot the high and low temperatures.
 plt.style.use('Solarize_Light2')
@@ -30,7 +35,7 @@ ax.plot(dates, lows, c='blue', linewidth=0.8, alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-plt.title("Daily high and low temperatures - 2021\nDeath Valley, CA", fontsize=14)
+plt.title(f"Daily high and low temperatures - 2021\n{station}", fontsize=13)
 plt.xlabel('', fontsize=13)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=13)
--- exersices/16-1. Sitka Rainfall.py
+++ exersices/16-1. Sitka Rainfall.py
@@ -7,26 +7,32 @@ with open("../data/sitka_weather_2021_full.csv") as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
+    headers = {}
+    for index, column in enumerate(header_row):
+        headers[column] = index
+
     dates, rainfall_amounts = [], []
     for row in reader:
-        date = datetime.strptime(row[2], '%Y-%m-%d')
+        date = datetime.strptime(row[headers['DATE']], '%Y-%m-%d')
         try:
-            rainfall_amount = float(row[5])
+            rainfall_amount = float(row[headers['PRCP']])
         except ValueError:
             print(f'Missing data for {date}')
         else:
             dates.append(date)
             rainfall_amounts.append(rainfall_amount)
+    station = row[headers['NAME']]
 
 
 plt.style.use('ggplot')
 fig, ax = plt.subplots()
 ax.plot(dates, rainfall_amounts, c='blue', linewidth=1)
 
-plt.title("Rainfall Amounts in 2021 - Sitka, Alaska", fontsize=16)
+plt.title(f"Rainfall Amounts in 2021 - \n{station}", fontsize=14)
 plt.xlabel('', fontsize=13)
 fig.autofmt_xdate()
 plt.ylabel('Rainfall Amounts', fontsize=13)
 plt.tick_params(axis='both', which='major', labelsize=13)
+plt.ylim(-0.2, 3.0)
 
 plt.show()
