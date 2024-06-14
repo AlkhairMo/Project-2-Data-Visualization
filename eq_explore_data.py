7a9ca7e2ac7be4d7056ee4c17ade2b6f1b48d811
alkhair <alkhair.mohammd@gmail.com>
2024-06-14 14:53:31 +0200

    customizing marker color and adding a hover text from earthquake title

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -10,21 +10,28 @@ with open(filename) as f:
 
 all_eq_dicts = all_eq_data['features']
 
-megs, lons, lats = [], [], []
+megs, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
     meg = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     megs.append(meg)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
 # Map the earthquakes
 data = [{'type': 'scattergeo',
          'lon': lons,
          'lat': lats,
+         'text': hover_texts,
          'marker': {
              'size': [3*meg for meg in megs],
+             'color': megs,
+             'colorscale': 'Hot',
+             'reversescale': True,
+             'colorbar': {'title': 'Magnitude'},
          },
          }]
 my_layout = Layout(title="Global Earthquakes")
