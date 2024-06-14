a66da96d04ced5ef37ba15cf9e5fda9b06b348b0
alkhair <alkhair.mohammd@gmail.com>
2024-06-14 15:02:48 +0200

    refactoring earthquake file using list comprehensions

--- eq_world_map.py
+++ eq_world_map.py
@@ -10,16 +10,10 @@ with open(filename) as f:
 
 all_eq_dicts = all_eq_data['features']
 
-megs, lons, lats, hover_texts = [], [], [], []
-for eq_dict in all_eq_dicts:
-    meg = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    title = eq_dict['properties']['title']
-    megs.append(meg)
-    lons.append(lon)
-    lats.append(lat)
-    hover_texts.append(title)
+megs = [eq_dict['properties']['mag'] for eq_dict in all_eq_dicts]
+lons = [eq_dict['geometry']['coordinates'][0] for eq_dict in all_eq_dicts]
+lats = [eq_dict['geometry']['coordinates'][1] for eq_dict in all_eq_dicts]
+hover_texts = [eq_dict['properties']['title'] for eq_dict in all_eq_dicts]
 
 # Map the earthquakes
 data = [{'type': 'scattergeo',
