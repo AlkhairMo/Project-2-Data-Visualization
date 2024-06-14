ff6483a96d060694ad57dc3bbf6cfad71ba2d05e
alkhair <alkhair.mohammd@gmail.com>
2024-06-14 16:48:16 +0200

    automatic title to make the code work with any world fires data

--- exersices/16-9. World Fires.py
+++ exersices/16-9. World Fires.py
@@ -3,7 +3,7 @@ import csv
 from plotly.graph_objs import Layout
 from plotly import offline
 
-filename = '../data/world_fires_1_day.csv'
+filename = '../data/world_fires_7_day.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -12,14 +12,22 @@ with open(filename) as f:
     for index, column in enumerate(header_row):
         headers[column] = index
 
-    lats, lons, brts = [], [], []
+    lats, lons, brts, dates = [], [], [], []
     for row in reader:
         lat = row[headers['latitude']]
         lon = row[headers['longitude']]
         brt = row[headers['brightness']]
+        date = row[headers['acq_date']]
         lats.append(float(lat))
         lons.append(float(lon))
         brts.append(float(brt))
+        dates.append(date)
+    first_date = dates[0]
+    last_date = dates[-1]
+    if first_date == last_date:
+        date_info = f'in {first_date}'
+    else:
+        date_info = f'between {first_date} and {last_date}'
 
 data = [{'type': 'scattergeo',
          'lon': lons,
@@ -33,6 +41,6 @@ data = [{'type': 'scattergeo',
              },
          },
          }]
-my_layout = Layout(title=f"World Fires in {row[headers['acq_date']]}")
+my_layout = Layout(title=f"World Fires {date_info}")
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='world_fires.html')
