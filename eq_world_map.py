f2befcfbbea8008ac65a66b7f577bc30edeb64c6
alkhair <alkhair.mohammd@gmail.com>
2024-06-14 15:09:48 +0200

    automated title for our map

--- eq_world_map.py
+++ eq_world_map.py
@@ -9,7 +9,9 @@ with open(filename) as f:
     all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data['features']
+eq_fileinfo_dic = all_eq_data['metadata']
 
+title = eq_fileinfo_dic['title']
 megs = [eq_dict['properties']['mag'] for eq_dict in all_eq_dicts]
 lons = [eq_dict['geometry']['coordinates'][0] for eq_dict in all_eq_dicts]
 lats = [eq_dict['geometry']['coordinates'][1] for eq_dict in all_eq_dicts]
@@ -28,7 +30,7 @@ data = [{'type': 'scattergeo',
              'colorbar': {'title': 'Magnitude'},
          },
          }]
-my_layout = Layout(title="Global Earthquakes")
+my_layout = Layout(title=title)
 
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='global_earthquakes.html')
