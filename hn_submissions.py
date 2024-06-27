a693b8b2460547ab5b65cceaf63dbe5a15bee20e
alkhair <alkhair.mohammd@gmail.com>
2024-06-27 18:44:11 +0200

    17-2. Active Discussions exercise

--- hn_submissions.py
+++ hn_submissions.py
@@ -1,4 +1,5 @@
 from operator import itemgetter
+from plotly import offline
 
 import requests
 
@@ -28,7 +29,19 @@ for submission_id in submission_ids[:28]:
 
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
                           reverse=True)
+discussion_links, comments = [], []
 for submission_dict in submission_dicts:
-    print(f"\nTitle: {submission_dict['title']}"
-          f"\nDiscussion link: {submission_dict['hn_link']}"
-          f"\nComments: {submission_dict['comments']}")
+    discussion_url = submission_dict['hn_link']
+    title = submission_dict['title']
+    discussion_link = f"<a href='{discussion_url}'>{title}</a>"
+    discussion_links.append(discussion_link)
+    comment = submission_dict['comments']
+    comments.append(comment)
+
+# Make the bar.
+data = [{'type': 'bar',
+         'x': discussion_links,
+         'y': comments}]
+my_layout = {'title': "Most active discussions currently in Hacker News"}
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename="hn_most_active.html")
